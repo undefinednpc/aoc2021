e118b1c371ea95339a8c5f181e5fb34568ec9ff0
undefinednpc <50375250+undefinednpc@users.noreply.github.com>
2021-12-06 16:11:40 +0100

    added comments to better understantd the code

--- day1/day1.ml
+++ day1/day1.ml
@@ -1,7 +1,11 @@
 let read_lines name : int list =
+  (* opening the channel *)
   let ic = open_in name in
+  (* reading helper function, return a line s or EOF *)
   let try_read () =
     try Some (input_line ic) with End_of_file -> None in
+  (* appends lines converted to int to the list *)
+  (* the loop matches the result of try_read () *)
   let rec loop acc = match try_read () with
     | Some s -> loop (int_of_string s :: acc)
     | None -> close_in ic; List.rev acc in
@@ -9,12 +13,14 @@ let read_lines name : int list =
 
 let result a b = if a < b then 1 else 0
 
+(* this function takes the list, compare the first two numbers and then calls itself to compare next numbers *)
 let rec increasing acc l =
   match l with
   | [] -> acc
   | [only] -> acc
   | first :: second :: rest -> increasing (acc + result first second) (second :: rest)
 
+(* same, but it convers the window *)
 let rec increasing_window acc l =
   match l with
   | [] -> acc
